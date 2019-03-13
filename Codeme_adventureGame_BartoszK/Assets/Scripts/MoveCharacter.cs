badbf784e821a19c19d2424eb0379aa64805273c
Hicmade <hicmade@zilla-oms.cba.pl>
2019-03-13 20:55:56 +0100

    First scene set

---
+++ Codeme_adventureGame_BartoszK/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float speed = 1000f;
+    public float rotSpeed = 800f;
+    private Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotSpeed;
+       // float strafe = Input.GetAxis("Horizontal2") * speed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        //strafe *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        //playerRB.AddRelativeForce(0, 0, translation);
+        //playerRB.AddRelativeTorque(0, rotation, 0);
+    }
+}

86176c265212c6dfa62333b9852f5807b1315cc5
Hicmade <hicmade@zilla-oms.cba.pl>
2019-03-16 19:23:57 +0100

    *Final scene arrangement *Added enemy moving script (rotation to be corrected)

--- Codeme_adventureGame_BartoszK/Assets/Scripts/MoveCharacter.cs
+++ Codeme_adventureGame_BartoszK/Assets/Scripts/MoveCharacter.cs
@@ -6,29 +6,21 @@ public class MoveCharacter : MonoBehaviour
 {
     public float speed = 1000f;
     public float rotSpeed = 800f;
-    private Rigidbody playerRB;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerRB = gameObject.GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotSpeed;
-       // float strafe = Input.GetAxis("Horizontal2") * speed;
 
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
-        //strafe *= Time.deltaTime;
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
 
-        //playerRB.AddRelativeForce(0, 0, translation);
-        //playerRB.AddRelativeTorque(0, rotation, 0);
+        if (Input.GetKeyDown(KeyCode.I)) {
+            Debug.Log("Translation: " + translation);
+            Debug.Log("Rotation: " + rotation);
+        }
     }
 }
